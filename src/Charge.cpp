0db506cfbc5837a18b62f84c63b86499dd316425
Maneren <maneren731@gmail.com>
2024-10-04 02:07:46 +0200

    global scale

--- src/Charge.cpp
+++ src/Charge.cpp
@@ -1,4 +1,5 @@
 #include "Charge.hpp"
+#include "defs.hpp"
 #include "utils.hpp"
 #include <raymath.h>
 
@@ -6,7 +7,7 @@ void Charge::update(float timeDelta) { _strength = strengthFn(timeDelta); }
 
 void Charge::draw() {
   auto color = lerp(NEGATIVE, POSITIVE, Normalize(_strength, -4, 4));
-  _position.DrawCircle(_strength, color);
+  (_position * GLOBAL_SCALE).DrawCircle(10 * _strength, color);
 }
 
 const raylib::Color Charge::POSITIVE = raylib::Color(255, 0, 0, 255);
--- src/defs.hpp
+++ src/defs.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "raylib.h"
 #include <raylib-cpp.hpp>
 
 using uint = unsigned int;
@@ -8,4 +9,6 @@ static uint SCREEN_WIDTH = 800;
 static uint SCREEN_HEIGHT = 600;
 
 constexpr float EPSILON_0 = 8.8541878128e-12;
-constexpr float K_E = 1 / (4 * M_PI * EPSILON_0);
+constexpr float K_E = 1 / (4 * PI * EPSILON_0);
+
+constexpr float GLOBAL_SCALE = 10.0f;
