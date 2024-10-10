25c64c531ebe9f3d94e2c39bd5a73a2561ed9a18
Maneren <maneren731@gmail.com>
2024-10-10 16:00:14 +0200

    drop uint for size_t

--- src/defs.hpp
+++ src/defs.hpp
@@ -5,11 +5,9 @@
 #include <raylib-cpp.hpp>
 #include <unordered_map>
 
-using uint = unsigned int;
-
 // Global screen dimensions, updated on window resize
-static uint SCREEN_WIDTH = 800;
-static uint SCREEN_HEIGHT = 600;
+static size_t SCREEN_WIDTH = 800;
+static size_t SCREEN_HEIGHT = 600;
 
 constexpr float EPSILON_0 = 8.8541878128e-12;
 constexpr float K_E = 1 / (4 * PI * EPSILON_0);
