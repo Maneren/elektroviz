65e8d6bcd4c80f58aef68b482e1ca4388c7435c1
Maneren <maneren731@gmail.com>
2024-10-16 01:03:10 +0200

    refactor field to ranges

--- src/field.cpp
+++ src/field.cpp
@@ -1,25 +1,25 @@
 #include "field.hpp"
 #include "defs.hpp"
+#include <algorithm>
+#include <functional>
+#include <ranges>
 
 namespace field {
 
+namespace ranges = std::ranges;
+namespace views = std::views;
+
 raylib::Vector2 E(const raylib::Vector2 point,
                   const std::vector<Charge> &charges) {
-  raylib::Vector2 result{0.f};
-  for (const auto &charge : charges) {
-    result += charge.E(point);
-  }
-  return result * K_E;
+  auto fields = views::transform(charges, [&](auto &c) { return c.E(point); });
+  return ranges::fold_left(fields, raylib::Vector2{0.f}, std::plus<>()) * K_E;
 }
 
 float potential(const raylib::Vector2 point,
                 const std::vector<Charge> &charges) {
-  float result = 0.0f;
-  for (const auto &charge : charges) {
-    result += charge.potential(point);
-  }
-
-  return result * K_E;
+  auto potentials =
+      views::transform(charges, [&](auto &c) { return c.potential(point); });
+  return ranges::fold_left(potentials, 0.f, std::plus<>()) * K_E;
 }
 
 } // namespace field
